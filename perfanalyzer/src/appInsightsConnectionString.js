6a99eff85db0be9139b578d66056d2c0ebb345f4
Prateek Mishra <pramishr@microsoft.com>
2023-01-22 02:12:59 +0530

    Updated gitignore

---
+++ perfanalyzer/src/appInsightsConnectionString.js
@@ -0,0 +1,7 @@
+"use strict";
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+exports.__esModule = true;
+exports.APPINSIGHTS_CONNECTION_STRING = void 0;
+//export const APPINSIGHTS_CONNECTION_STRING='InstrumentationKey=299382e8-b5d3-4d1d-98bb-a4c1ccca1165;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/'
+exports.APPINSIGHTS_CONNECTION_STRING = 'InstrumentationKey=6ecac06a-d5cb-46d7-abe1-a411937bb623;IngestionEndpoint=https://southeastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/';
